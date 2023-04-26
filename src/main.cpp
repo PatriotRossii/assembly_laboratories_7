2349c88294848a0746c176250e706f8833c1f918
PatriotRossii <patriotrossii2019@mail.ru>
2023-04-26 13:43:50 +0300

    Simplify code by moving size to constexpr variable

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-#define size(array, type) sizeof(array)/sizeof(type)
-
 extern "C" int ArrayMaximum_(
 	int array[], int length
 );
@@ -24,28 +22,29 @@ static void PrintResult(
 }
 
 int main() {
+	constexpr int SIZE = 10;
 	{
 		int array[] = {
 			6, 58, 53, 70, 74, 89, 25, 90, 34, 26
 		};
-		int maximum = ArrayMaximum_(array, size(array, int));
-		int minimum = ArrayMinimum_(array, size(array, int));
+		int maximum = ArrayMaximum_(array, SIZE);
+		int minimum = ArrayMinimum_(array, SIZE);
 		PrintResult("Пример 1", maximum, minimum);
 	}
 	{
 		int array[] = {
 			84, 66, 24, 87, 22, 78, 72, 41, 83, 21
 		};
-		int maximum = ArrayMaximum_(array, size(array, int));
-		int minimum = ArrayMinimum_(array, size(array, int));
+		int maximum = ArrayMaximum_(array, SIZE);
+		int minimum = ArrayMinimum_(array, SIZE);
 		PrintResult("Пример 2", maximum, minimum);
 	}
 	{
 		int array[] = {
 			52, 40, 65, 62, 1, 39, 10, 18, 30, 98
 		};
-		int maximum = ArrayMaximum_(array, size(array, int));
-		int minimum = ArrayMinimum_(array, size(array, int));
+		int maximum = ArrayMaximum_(array, SIZE);
+		int minimum = ArrayMinimum_(array, SIZE);
 		PrintResult("Пример 3", maximum, minimum);
 	}
 }
